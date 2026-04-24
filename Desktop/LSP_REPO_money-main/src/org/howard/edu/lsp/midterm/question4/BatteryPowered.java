6dbdea8bdf7f816e71e491409f28b28581709a57
trishmag <misslittletrish@gmail.com>
2026-04-24 11:11:26 -0400

    Delete Desktop/LSP_REPO_money-main directory

--- Desktop/LSP_REPO_money-main/src/org/howard/edu/lsp/midterm/question4/BatteryPowered.java
+++
@@ -1,7 +0,0 @@
-package org.howard.edu.lsp.midterm.question4;
-
-public interface BatteryPowered {
-    int getBatteryPercent();
-    void setBatteryPercent(int percent);
-}
-
--- Desktop/LSP_REPO_money-main/src/org/howard/edu/lsp/midterm/question4/Networked.java
+++
@@ -1,6 +0,0 @@
-package org.howard.edu.lsp.midterm.question4;
-public interface Networked {
-    void connect();
-    void disconnect();
-    boolean isConnected();
-}
